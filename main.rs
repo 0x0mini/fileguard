f2e04dfa100d30a5c454ef3f2ee560a5c8e59aad
0x0mini <163350822+0x0mini@users.noreply.github.com>
2024-05-16 21:11:28 +0200

    cleaning code

--- main.rs
+++ main.rs
@@ -28,34 +28,45 @@ async fn process_file(payload: &mut Multipart, tool_path: &str, success_message:
             Ok(f) => f,
             Err(_) => return HttpResponse::BadRequest().body("Invalid file upload"),
         };
-        let filepath = match field.content_disposition().get_filename() {
-            Some(name) => format!("./{}", name),
-            None => return HttpResponse::BadRequest().body("Filename missing from content disposition"),
-        };
-        let mut f = match File::create(&filepath) {
-            Ok(file) => file,
-            Err(_) => return HttpResponse::InternalServerError().body("Failed to create file"),
-        };
 
-        while let Some(chunk) = field.next().await {
-            match chunk {
-                Ok(data) => {
-                    if let Err(_) = f.write_all(&data) {
-                        return HttpResponse::InternalServerError().body("Failed to write file");
-                    }
-                }
-                Err(_) => return HttpResponse::BadRequest().body("Error in file chunks"),
-            };
+        let filepath = field.content_disposition().get_filename()
+            .map(|name| format!("./{}", name))
+            .ok_or_else(|| HttpResponse::BadRequest().body("Filename missing from content disposition"))?;
+
+        let mut file = File::create(&filepath)
+            .map_err(|_| HttpResponse::InternalServerError().body("Failed to create file"))?;
+
+        if let Err(_) = write_file(&mut field, &mut file).await {
+            return HttpResponse::BadRequest().body("Failed to write file");
         }
 
-        let output = match Command::new(tool_path).arg(&filepath).output() {
-            Ok(output) if output.status.success() => return HttpResponse::Ok().body(success_message),
-            Ok(_) => return HttpResponse::InternalServerError().body(error_message),
-            Err(_) => return HttpResponse::InternalServerError().body("Failed to execute process"),
-        };
+        if let Err(response) = execute_tool(tool_path, &filepath, success_message, error_message){
+            return response;
+        }
     }
 
     HttpResponse::BadRequest().body("No file uploaded")
+}
+
+async fn write_file(field: &mut actix_multipart::Field, file: &mut File) -> io::Result<()> {
+    while let Some(chunk) = field.next().await {
+        let data = chunk?;
+        file.write_all(&data)?;
+    }
+    Ok(())
+}
+
+fn execute_tool(tool_path: &str, filepath: &str, success_message: &str, error_message: &str) -> Result<(), HttpResponse> {
+    let output = Command::new(tool_path)
+        .arg(filepath)
+        .output();
+
+    match output {
+        Ok(output) if output.status.success() => Ok(()),
+        Ok(_) => Err(HttpResponse::InternalServerError().body(error_message)),
+        Err(_) => Err(HttpResponse::InternalServerError().body("Failed to execute process")),
+    }.map_err(|resp| resp)
+     .or_else(|_| Ok(HttpResponse::Ok().body(success_message)))
 }
 
 #[actix_web::main]
@@ -65,10 +76,10 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
-            .data(AppState {
+            .app_data(web::Data::new(AppState {
                 encrypt_tool_path: encrypt_tool_path.clone(),
                 decrypt_tool_path: decrypt_tool_path.clone(),
-            })
+            }))
             .wrap(middleware::Logger::default())
             .service(encrypt)
             .service(decrypt)
